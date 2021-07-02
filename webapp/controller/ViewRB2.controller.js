f7708d76e1c9ddb996cb92edafea108eaaf8eb5e
Roberto-Bahamondes <peteto2004@hotmail.com>
2021-07-02 22:28:17 +0000

    Segunda Actualizacion

--- webapp/controller/ViewRB2.controller.js
+++ webapp/controller/ViewRB2.controller.js
@@ -8,16 +8,11 @@ sap.ui.define([
 		"use strict";
 
 		return Controller.extend("namespacerb2.projectrb2.controller.ViewRB2", {
-			onInit: function () {
-                //Vamos a crear una instancia del Modelo
-                const oJSONModel = new sap.ui.model.json.JSONModel();
-                //Vamos a obtener la instancia de la vista
-                const oView = this.getView();
-                //Vamos a cargar los datos que estan en la ruta al Modelo
-                oJSONModel.loadData("./model/SelectionScreenMenu.json");
-                //Vamos crear un modelo llamado selectionScreen 
+			onInit: function () {               
+                const oJSONModel = new sap.ui.model.json.JSONModel();               
+                const oView = this.getView();               
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");               
                 oView.setModel(oJSONModel, "selectionScreen");
-
 			}
 		});
 	});
